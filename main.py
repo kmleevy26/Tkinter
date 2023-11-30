d4faf80ab1d88bf8b7cb62600691a0e23774e816
kmleevy26 <98064292+kmleevy26@users.noreply.github.com>
2023-11-29 19:15:42 -0600

    Show message function how displays messagebox when checked

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class MyGUI:
 
+    # Intial function
     def __init__(self):
 
+        # Root
         self.root = tk.Tk()
         
-
+        # Label
         self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
         self.label.pack(padx=10, pady=10)
 
+        # Textbox
         self.textbox = tk.Text(self.root, font=('Arial', 18), height=5)
         self.textbox.pack(padx=10, pady=10,)
+        self.textbox.bind("<KeyPress>", self.shortcut)
 
+        # State
         self.check_state = tk.IntVar()
-
+        
+        # Checkbox
         self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 18), variable=self.check_state)
         self.check.pack(padx=10, pady=10)
         
+        # Button
         self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
         self.button.pack(padx=10, pady=10)
         
 
         self.root.mainloop()
 
+    # Show message function
     def show_message(self):
-        print("Hello World")
-
-
-MyGUI()
+        if self.check_state.get() == 0:
+            print(self.textbox.get('1.0', tk.END))
+        else:
+            messagebox.showinfo(title="Message", message=self.textbox.get('1.0', tk.END))
